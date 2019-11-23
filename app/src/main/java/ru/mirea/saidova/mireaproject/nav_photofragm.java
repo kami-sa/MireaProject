ea912b1e7a87c8ae37122a0492e7ec69a9a2a583
kami-sa <kamisa1999@yandex.ru>
2019-11-23 11:47:00 +0300

    Add work with a camera

--- app/src/main/java/ru/mirea/saidova/mireaproject/nav_photofragm.java
+++ app/src/main/java/ru/mirea/saidova/mireaproject/nav_photofragm.java
@@ -1,13 +1,31 @@
 package ru.mirea.saidova.mireaproject;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -15,7 +33,7 @@ import android.view.ViewGroup;
  * Use the {@link nav_photofragm#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class nav_photofragm extends Fragment {
+public class nav_photofragm extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,7 +42,14 @@ public class nav_photofragm extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private static final int REQUEST_CODE_PERMISSION_CAMERA = 100;
+    final String TAG = MainActivity.class.getSimpleName();
+    private ImageView imageView;
+    private static final int CAMERA_REQUEST = 0;
+    File file;
+    FileOutputStream fileoutputstream;
+    ByteArrayOutputStream bytearrayoutputstream;
+    private boolean isWork = false;
 
     public nav_photofragm() {
         // Required empty public constructor
@@ -60,8 +85,66 @@ public class nav_photofragm extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nav_photofragm, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_photofragm, container, false);
+        imageView = view.findViewById(R.id.imageView);
+        bytearrayoutputstream = new ByteArrayOutputStream();
+        int permissionStatus = ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CAMERA);
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_PERMISSION_CAMERA);
+        }
+        imageView.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+// извлекаем изображение
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            bitmap.compress(Bitmap.CompressFormat.PNG, 60, bytearrayoutputstream);
+            imageView.setImageBitmap(bitmap);
+            try
+
+            {
+                fileoutputstream = new FileOutputStream(Environment.getExternalStorageDirectory() + "/SampleImage.png");
+                fileoutputstream.write(bytearrayoutputstream.toByteArray());
+                fileoutputstream.close();
+                Log.d(TAG, file.getAbsolutePath());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            Toast.makeText(getContext(), "Сохранили картинку. Ищи во внутренней памяти.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSION_CAMERA) {
+            Log.d(TAG, String.valueOf(grantResults[0]) + " " + String.valueOf(grantResults[1]));
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+// permission granted
+                isWork = true;
+            } else {
+                isWork = false;
+            }
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+    }
 }
